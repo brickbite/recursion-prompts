33806cdbb20f18174bf6baa8bdec5ce59115b19f
brickbite <jeffreylee0116@gmail.com>
2018-04-03 13:35:15 -0700

    completed range

--- src/recursion.js
+++ src/recursion.js
@@ -85,6 +85,21 @@ var sumBelow = function(n) {
 // 6. Get the integers within a range (x, y).
 // range(2,9); // [3,4,5,6,7,8]
 var range = function(x, y) {
+  if (Math.abs(y - x) <= 1) {
+    return [];
+  }
+
+  if (x < y) {
+    let a = [x + 1];
+    a = a.concat(range(x + 1, y));
+    return a;
+  }
+
+  if (x > y) {
+    let a = [x - 1];
+    a = a.concat(range(x - 1, y));
+    return a;
+  }
 };
 
 // 7. Compute the exponent of a number.
